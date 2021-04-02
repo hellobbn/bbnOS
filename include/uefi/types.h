7c09543ccf84ed3c2ba1f7090ab183eab2173d4f
Luofan Chen <clfbbn@gmail.com>
2021-04-02 14:25:19 +0800

    [types] Use std includes
    Because they don't contain std functions, so that we can safely
    use them without error
    
    Signed-off-by: Luofan Chen <clfbbn@gmail.com>

--- include/uefi/types.h
+++ include/uefi/types.h
@@ -12,24 +12,14 @@
 #ifndef TYPE_H
 #define TYPE_H
 
-typedef int int_t;
-typedef long long_t;
+#include <stdint.h>
+#include <stddef.h>
 
-typedef signed int int32_t;
-typedef unsigned int uint32_t;
-typedef signed short int16_t;
-typedef unsigned short uint16_t;
-typedef signed char int8_t;
-typedef unsigned char uint8_t;
-typedef unsigned long long uint64_t;
-typedef unsigned long long size_t;
-
+#ifndef __cplusplus
 // BOOL type, which is char
 typedef char bool;
 #define true 1
 #define false 0
-
-// NULL
-#define NULL 0
+#endif
 
 #endif // TYPE_H
